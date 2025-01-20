5de593390a6885ca0d4dc728f1aecac57f3aafb2
Parastou-tech <Parastou.fard@gmail.com>
2025-01-19 17:33:24 -0800

    rest api implementation

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -46,20 +46,32 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+const findUserByJob = (job) => 
+    users["users_list"].find((user)=> user["job"]===job);
+ 
 app.get("/users", (req, res) => {
   const name = req.query.name;
+  const job = req.query.job;
+
   if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
-  } else {
-    res.send(users);
   }
+  if( job != undefined){
+    let result = findUserByJob(job);
+    result = {users_list: result};
+    res.send(result)
+  }
+  
+res.send(users);
+  
 });
 
-const findUserById = (id) =>
-    users["users_list"].find((user) => user["id"] === id);
-  
+ 
 app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
     let result = findUserById(id);
@@ -68,6 +80,29 @@ app.get("/users/:id", (req, res) => {
     } else {
       res.send(result);
     }
+  });
+
+  const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
+
+  app.delete("/users/:id", (req, res)=>{
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+      } else {
+        users.users_list.splice(result, 1);
+        res.status(204).send();
+      }
   });
 
 app.listen(port, () => {
