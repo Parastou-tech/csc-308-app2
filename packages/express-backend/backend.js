e0cb0c0a42e2031d6c4ac72df966db30b7a71de3
Parastou-tech <Parastou.fard@gmail.com>
2025-01-21 21:28:52 -0800

    link-step 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -35,6 +36,8 @@ const users = {
   };
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
