1b65c8d6d11ad86a3055cb4ebb496b44fb89f01b
Parastou-tech <Parastou.fard@gmail.com>
2025-01-28 21:05:02 -0800

    2.2 & 2.3

--- tdd/portfolio.js
+++ tdd/portfolio.js
@@ -6,6 +6,30 @@ class StockPortfolio{
     isEmpty() {
         return Object.keys(this.portfolio).length === 0;
     }
+
+    purchase(ticker, shares){
+        if (!ticker || shares <= 0){
+            throw new Error('Invalid ticker symbol or number of shares.');
+        }
+        if (this.portfolio[ticker]){
+            this.portfolio[ticker] += shares;
+        } else{
+            this.portfolio[ticker] = shares;
+        }
+    }
+
+    getShares(ticker){
+        if(!ticker){
+            throw new Error('Invalid ticker symbol.');
+        }
+        if(this.portfolio[ticker]){
+            return this.portfolio[ticker];
+        } else {
+            return 0;
+        }
+    }
+
+    
 }
 
 module.exports = StockPortfolio;
--- tdd/portfolio.test.js
+++ tdd/portfolio.test.js
@@ -6,9 +6,24 @@ describe('StockPortfolio Module - TDD Implementation', () => {
         portfolio = new StockPortfolio()
     });
 
-    //2.1
+    //2.1 
     test('Portfolio should initialize empty', ()=> {
         expect(portfolio.isEmpty()).toBe(true);
     });
+
+    //2.2
+    test('Portfolio reports does not report as empty when shares are bought', ()=> {
+        expect(portfolio.isEmpty()).toBe(true);
+        portfolio.purchase('AMZN', 5);
+        expect(portfolio.isEmpty()).toBe(false);
+    })
+
+    //2.3
+    test('Portfolio shows # of shares for a given company.', () => {
+        portfolio.purchase('AMZN', 5);
+        expect(portfolio.getShares('AMZN')).toBe(5);
+        portfolio.purchase('AMZN', 3);
+        expect(portfolio.getShares('AMZN')).toBe(8);
+    })
 });
 
